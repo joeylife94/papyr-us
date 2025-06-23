c7ecaccefcd10b38057df94c84c7457300e813be
joey <sho801921@gmail.com>
2025-06-23 19:01:06 +0900

    Fix: wouter routes with /papyr-us base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -50,28 +50,49 @@ function Router() {
         <main className="flex-1 lg:ml-80">
           <div className="max-w-7xl mx-auto">
             <Switch>
-              <Route path="/">
+              {/* --- 여기부터 Route path 수정! --- */}
+
+              {/* 기본 경로: /papyr-us/ */}
+              <Route path="/papyr-us/"> {/* <-- /papyr-us/ 경로를 명시! */}
                 <Home 
                   searchQuery={searchQuery}
                   selectedTags={selectedTags}
                   selectedFolder={selectedFolder}
                 />
               </Route>
-              <Route path="/page/:slug" component={WikiPageView} />
-              <Route path="/calendar/:teamId">
+              
+              {/* /papyr-us/page/:slug */}
+              <Route path="/papyr-us/page/:slug" component={WikiPageView} />
+              
+              {/* /papyr-us/calendar/:teamId */}
+              <Route path="/papyr-us/calendar/:teamId">
                 {(params) => <CalendarPage teamId={params.teamId} />}
               </Route>
-              <Route path="/edit/:pageId">
+              
+              {/* /papyr-us/edit/:pageId */}
+              <Route path="/papyr-us/edit/:pageId">
                 {(params) => <PageEditor pageId={params.pageId} />}
               </Route>
-              <Route path="/create">
+              
+              {/* /papyr-us/create */}
+              <Route path="/papyr-us/create">
                 <PageEditor />
               </Route>
-              <Route path="/create/:folder">
+              
+              {/* /papyr-us/create/:folder */}
+              <Route path="/papyr-us/create/:folder">
                 {(params) => <PageEditor initialFolder={params.folder} />}
               </Route>
-              <Route path="/admin" component={AdminPage} />
+              
+              {/* /papyr-us/admin */}
+              <Route path="/papyr-us/admin" component={AdminPage} />
+              
+              {/* Catch-all for /papyr-us/ 에 해당하지 않는 경로 (예: /papyr-us/abc -> NotFound) */}
+              {/* Nginx가 이미 /papyr-us/ 를 잡고 있으므로, 이 NotFound는 /papyr-us/ 하위에서 매칭 안 되는 경우에 뜸 */}
               <Route component={NotFound} />
+
+              {/* 만약 / (루트)로 접속했을 때도 NotFound가 뜨면, Nginx에서 /papyr-us/ 로 리다이렉트하는 설정은 그대로 유지 */}
+              {/* 또는 / 라우트를 추가할 수 있지만, Nginx가 이미 리다이렉트하므로 불필요할 가능성 높음 */}
             </Switch>
           </div>
         </main>
